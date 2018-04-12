41f3d1d9c68a65a4f6d12ab711c654532139da8b
songmilee <lsm941010@gmail.com>
2018-04-12 11:23:11 +0900

    change welcome msg and contact section

--- js/main.js
+++ js/main.js
@@ -1,28 +1,21 @@
 $(document).ready(function(){
    var scroll_start = 0;
-   var startchange = $('#who');
+   var startchange = $('#project');
    var offset = startchange.offset();
+
+   $('#welcome_msg').css('display', 'none').fadeIn(3000);
     if (startchange.length){
-   $(document).scroll(function() {
-      scroll_start = $(this).scrollTop();
-      if(scroll_start > offset.top - 40) {
-        //$(".navbar-font").css('color', 'black');
-        $(".navbar-main").css('background-color', 'rgba(0, 0, 0, 1.0)');
-        $(".navbar-main").css('border', 'solid 1px rgba(225, 225, 225, 1.0)');
-       } else {
-         //$(".navbar-font").css('color', 'white');
-         $('.navbar-main').css('background-color', 'transparent');
-         $(".navbar-main").css('border', 'none');
-       }
-   });
+       $(document).scroll(function() {
+          scroll_start = $(this).scrollTop();
+          if(scroll_start > offset.top - 40) {
+            //$(".navbar-font").css('color', 'black');
+            $(".navbar-main").css('background-color', 'rgba(0, 0, 0, 1.0)');
+            $(".navbar-main").css('border', 'solid 1px rgba(225, 225, 225, 1.0)');
+           } else {
+             //$(".navbar-font").css('color', 'white');
+             $('.navbar-main').css('background-color', 'transparent');
+             $(".navbar-main").css('border', 'none');
+           }
+       });
     }
 });
-
-function showText(target, message, index, interval){
-  if(index < message.length){
-    $(target).append(message[index++]);
-    setTimeout(function(){
-      showText(target, message, index, interval);
-    }, interval);
-  }
-}
