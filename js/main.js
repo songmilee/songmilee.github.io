97ccded59895a1562f298ed3cad5e7a23974124e
songmilee <lsm941010@gmail.com>
2018-04-13 12:51:50 +0900

    clean the mobile view & set the proto project box type

--- js/main.js
+++ js/main.js
@@ -8,11 +8,9 @@ $(document).ready(function(){
        $(document).scroll(function() {
           scroll_start = $(this).scrollTop();
           if(scroll_start > offset.top - 40) {
-            //$(".navbar-font").css('color', 'black');
-            $(".navbar-main").css('background-color', 'rgba(0, 0, 0, 1.0)');
-            $(".navbar-main").css('border', 'solid 1px rgba(225, 225, 225, 1.0)');
+            $(".navbar-main").css('background-color', 'rgba(153, 153, 204, 1.0)');
+            $(".navbar-main").css('border', 'solid 1px rgba(153, 153, 204, 1.0)');
            } else {
-             //$(".navbar-font").css('color', 'white');
              $('.navbar-main').css('background-color', 'transparent');
              $(".navbar-main").css('border', 'none');
            }
